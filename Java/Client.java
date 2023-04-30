668af4994fdc14f75c19edeb4e4166141685b8f3
ssexton1 <63212412+ssexton1@users.noreply.github.com>
2023-04-29 21:07:00 -0700

    Initial Work

--- Java/Client.java
+++ Java/Client.java
@@ -1,23 +1,60 @@
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 public class Client {
 
     /**
      * This method name and parameters must remain as-is
      */
     public static int add(int lhs, int rhs) {
-        return -1;
+        try (Socket socket = new Socket("localhost", 10314);
+        ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());) {
+            os.writeObject(new RemoteMethod("add", new Object[]{lhs, rhs}));
+            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+            return (int) is.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
+    
     /**
      * This method name and parameters must remain as-is
      */
-    public static int divide(int num, int denom) {
-        return -1;
+    public static int divide(int num, int denom) throws ArithmeticException {
+        try (Socket socket = new Socket("localhost", 10314);
+            ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());) {
+            os.writeObject(new RemoteMethod("divide", new Object[]{num, denom}));
+            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+            Object result = is.readObject();
+            try {
+                return (int) result;
+            } catch (ClassCastException e) {
+                throw new ArithmeticException();
+            }
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return -1;
+        }
     }
     /**
      * This method name and parameters must remain as-is
      */
     public static String echo(String message) {
-        return "";
+        try (Socket socket = new Socket("localhost", 10314);
+        ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());) {
+            os.writeObject(new RemoteMethod("echo", new Object[]{message}));
+            ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+            return (String) is.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
     }
+    
+    
 
     // Do not modify any code below this line
     // --------------------------------------
--- Java/Server.java
+++ Java/Server.java
@@ -1,4 +1,60 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
 public class Server {
+
+    private static ExecutorService exec = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(10314);
+            Socket socket;
+            while((socket = server.accept()) != null) {
+                Socket currSocket = socket;
+                Runnable request = () -> {
+                    try {
+                        handleRequest(currSocket);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                };
+                exec.execute(request);
+            }
+            server.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void handleRequest(Socket socket) throws IOException{
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        RemoteMethod remote_method = (RemoteMethod) in.readObject();
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        handleRemoteMethod(remote_method, out);
+        out.flush();
+        out.close();
+        in.close();
+    }
+
+    private static void handleRemoteMethod(RemoteMethod remote_method,  ObjectOutputStream out) throws IOException {
+        String method_name = remote_method.getMethodName();
+        Object[] args = remote_method.getArguments();
+        try {
+            if (method_name.equals("add")) {
+                out.writeObject(add((int)args[0], (int)args[1]));
+            } else if (method_name.equals("divide")) {
+                out.writeObject(divide((int)args[0], (int)args[1]));
+            } else if (method_name.equals("echo")) {
+                out.writeObject(echo((String)args[0]));
+            }
+        } catch (Exception ex) {
+            out.writeObject(ex); 
+        }
+    }
+     
+        
+    
 
     // Do not modify any code below tihs line
     // --------------------------------------
